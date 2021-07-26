28cb2be9aab028e072254120dec2e72dfbc0456a
aDunhamDSC <adrian_dunham@discovery.com>
2021-07-26 15:01:31 -0400

    Inital Commit

---
+++ bin/cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+console.log("Awesome Command");
+
+const {execSync} = require('child_process');
+
+const runCommand = (command) => {
+    try {
+        execSync(`${command}`, {stdio: 'inherit'})
+    } catch (error) {
+        console.error(`Failed to execute ${command}`, e);
+        return false;
+    }
+    return true
+}
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/hhimanshu/react-ts-starter ${repoName}`;
+const installDepsCommand = `cd ${repoName} &&  npm install`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if(!checkedOut) process.exit(-1);
+
+console.log(`Installing Dependencies for ${repoName}`);
+const installDeps = runCommand(installDepsCommand);
+if(!installDeps) process.exit(-1);
+
+console.log(`Congratulations! You are ready to start. Follow commands to start`);
+console.log(`cd ${repoName} && npm start`)
